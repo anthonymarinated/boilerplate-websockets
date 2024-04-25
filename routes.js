61cc81ce7d8db0a3bdd835a220db70629ccd88e6
anthonymarinated <anthonymarin718@gmail.com>
2024-04-25 13:59:06 -0400

    completed github authentication strategy

--- routes.js
+++ routes.js
@@ -10,7 +10,8 @@ module.exports = function (app, myDataBase) {
       title: 'Connected to Database',
       message: 'Please login',
       showLogin: true,
-      showRegistration: true
+      showRegistration: true,
+      showSocialAuth: true
     });
   });
 
@@ -62,6 +63,11 @@ module.exports = function (app, myDataBase) {
       res.redirect('/profile');
     }
   );
+
+  app.route('/auth/github').get(passport.authenticate('github'));
+  app.route('/auth/github/callback').get(passport.authenticate('github', { failureRedirect: '/'}), (req, res) => {
+    res.redirect('/profile');
+  });
 
   app.use((req, res, next) => {
     res.status(404).type('text').send('Not Found');
