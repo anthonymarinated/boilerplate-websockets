cedd7b7823a1013b8294d39e33bbb0a6fa60e54b
anthonymarinated <anthonymarin718@gmail.com>
2024-04-26 21:16:00 -0400

    modified socket emit event to announce new users

--- public/client.js
+++ public/client.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
   // Form submittion with new message in field with id 'm'
   let socket = io();
-
-  socket.on('user count', function(data) {
-    console.log(data);
-  });
   
+  socket.on('user', function(data){
+    $('#num-users').text(data.currentUsers + ' users online');
+    let message = 
+      data.username + (data.connected ? ' has joined the chat.' : ' has left the chat.');
+    $('#messages').append($('<li>').html('<b>' + message + '</b>'));    
+  });
+
   $('form').submit(function () {
     var messageToSend = $('#m').val();
 
